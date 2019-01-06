33c093e1f2cc874b7cafadeb8f0e1487314b29a6
metinsut <metindir@gmail.com>
2019-01-06 23:40:23 +0300

    edited

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,32 @@
-import React, { Fragment } from 'react';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
+import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
 class Header extends React.Component {
-
-    render() {
-
-        return (
-            <header>
-                <Link to="/">Redux Auth</Link>
-                {!this.props.auth ? (
-                    <Fragment>
-                        <Link to="/signup">Sign Up</Link>
-                        <Link to="/signin">Sign In</Link>
-                    </Fragment>
-                ) : (
-                    <Fragment>
-                        <Link to="/signout">Sign Out</Link>
-                        <Link to="/feature">Feature</Link>
-                    </Fragment>
-                )}
-            </header>
-        );
-    }
+   render() {
+      return (
+         <header>
+            <Link to="/">Redux Auth</Link>
+            {!this.props.auth ? (
+               <Fragment>
+                  <Link to="/signup">Sign Up</Link>
+                  <Link to="/signin">Sign In</Link>
+               </Fragment>
+            ) : (
+               <Fragment>
+                  <Link to="/signout">Sign Out</Link>
+               </Fragment>
+            )}
+            <Link to="/feature">Feature</Link>
+         </header>
+      );
+   }
 }
 
 const mapStateToProps = state => {
-    return {
-        auth: state.auth.success.user.token
-    };
+   return {
+      auth: state.auth.success.user.token
+   };
 };
 
 export default connect(mapStateToProps)(Header);
--- src/components/requireAuth.js
+++ src/components/requireAuth.js
@@ -1,30 +1,44 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
 export default ChildComponent => {
-    class ComposedComponent extends Component {
-        // Our component just got rendered
-        componentDidMount() {
+   class ComposedComponent extends Component {
+      state = {
+         active: false
+      };
+      // Our component just got rendered
+      componentDidMount() {
+         this.shouldNavigateAway();
+      }
+
+      componentDidUpdate(prevProps, prevState) {
+         if (this.props.auth.user.token !== prevProps.auth.user.token) {
+            console.log("go");
             this.shouldNavigateAway();
-        }
-        // Our component just got updated
-        componentDidUpdate() {
-            this.shouldNavigateAway();
-        }
-        shouldNavigateAway() {
-            if (this.props.auth.user.token === null) {
-                this.props.history.push('/');
-            }
-        }
-        render() {
-            return <ChildComponent {...this.props} />;
-        }
-    }
-    function mapStateToProps(state) {
-        return {
-            auth: state.auth.success,
-            state: state
-        };
-    }
-    return connect(mapStateToProps)(ComposedComponent);
+         }
+      }
+
+      shouldNavigateAway() {
+         if (this.props.auth.user.token === null) {
+            this.setState({ auth: false });
+            this.props.history.push("/");
+         } else {
+            this.setState({ auth: true });
+         }
+      }
+      render() {
+         return this.state.auth === true ? (
+            <ChildComponent {...this.props} />
+         ) : (
+            <div />
+         );
+      }
+   }
+   function mapStateToProps(state) {
+      return {
+         auth: state.auth.success,
+         state: state
+      };
+   }
+   return connect(mapStateToProps)(ComposedComponent);
 };
